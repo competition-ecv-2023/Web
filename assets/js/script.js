f6732906a80e84529f02b5f0f11e76988d0f4034
CoralyM <coraly.mgk@gmail.com>
2023-06-22 11:28:32 +0200

    Bug correction header.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,19 +17,21 @@ menuBtn.addEventListener("click", () => {
     }
 })
 
-menu.addEventListener("click", () => {
-    if(window.getComputedStyle(menu).display === "none") {
-        menu.style.display = "flex";      
-        menu.style.animation = "0.7s ease-in-out slidein";
-        menuBtn.classList.add("menu-open");
-    } else {
-        menu.style.animation = "0.7s ease-in-out slideout";
-        menuBtn.classList.remove("menu-open");
-        setTimeout(() => {
-            menu.style.display = "none";
-        }, 690)
-    }
-})
+if (window.matchMedia("(max-width: 1200px)").matches) {
+    menu.addEventListener("click", () => {
+        if(window.getComputedStyle(menu).display === "none") {
+            menu.style.display = "flex";      
+            menu.style.animation = "0.7s ease-in-out slidein";
+            menuBtn.classList.add("menu-open");
+        } else {
+            menu.style.animation = "0.7s ease-in-out slideout";
+            menuBtn.classList.remove("menu-open");
+            setTimeout(() => {
+                menu.style.display = "none";
+            }, 690)
+        }
+    })
+}
 
 /* SLIDER */
 
