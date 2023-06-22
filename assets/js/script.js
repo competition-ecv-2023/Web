6be5e61d73fd4fb3152b057e9b6315b1e9c83007
CoralyM <coraly.mgk@gmail.com>
2023-06-22 11:14:42 +0200

    Adding social media & bug correction.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,20 @@ menuBtn.addEventListener("click", () => {
     }
 })
 
+menu.addEventListener("click", () => {
+    if(window.getComputedStyle(menu).display === "none") {
+        menu.style.display = "flex";      
+        menu.style.animation = "0.7s ease-in-out slidein";
+        menuBtn.classList.add("menu-open");
+    } else {
+        menu.style.animation = "0.7s ease-in-out slideout";
+        menuBtn.classList.remove("menu-open");
+        setTimeout(() => {
+            menu.style.display = "none";
+        }, 690)
+    }
+})
+
 /* SLIDER */
 
 let testimoniesSlider = new Swiper(".testimonies-slider", {
