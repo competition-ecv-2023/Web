bc35cea7afa817d02185bb238aed1901ef4af415
CoralyM <coraly.mgk@gmail.com>
2023-06-22 10:31:34 +0200

    Adding style & animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-let menuBtn = document.querySelector(".header-navigation button");
+let menuBtn = document.querySelector(".header-navigation #menu-btn");
 let menu = document.querySelector(".header-navigation ul");
 
 menuBtn.addEventListener("click", () => {
     if(window.getComputedStyle(menu).display === "none") {
         menu.style.display = "flex";      
         menu.style.animation = "0.7s ease-in-out slidein";
+        menuBtn.classList.add("menu-open");
     } else {
         menu.style.animation = "0.7s ease-in-out slideout";
+        menuBtn.classList.remove("menu-open");
         setTimeout(() => {
             menu.style.display = "none";
         }, 690)
